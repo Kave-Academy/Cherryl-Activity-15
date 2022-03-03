707e56960756b64d5532e7e0b0a72d43e0fe4f07
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-03-03 14:55:43 +0800

    put a solution that display the sample data stated in inded.html

--- exer.js
+++ exer.js
@@ -1,3 +1,31 @@
-function run() {
+function run(){
+
+  document.getElementById('output').innerHTML =  
+  truncate('We are doing JS string exercises.') + " <br> " + 
+  truncate('We are doing JS string exercises.',19) +"<br> "+ 
+  truncate('We are doing JS string exercises.',15,'!!');
+
 }
+
+
+// First solution but the ... is not count as an array
+// function truncate(str, limit){
+//   if(str.length >= limit){
+//     return str.substring(0, limit) + "..."
+//   }else{
+//     return str;
+//   }
+// }
+
+
+//Shorten the code and put an additional condition
+
+function truncate(str, limit, ending) {
+  if (limit && ending == null) {
+    ending = '...';
+  }
+  if (str.length > limit) str = str.substring(0, limit - ending.length) + ending;
+  return str;
+};
+
 
