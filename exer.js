181425e9f39c5b30bbff6207e55b44f14bdd83c2
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-03-04 16:37:08 +0800

    additional tooltip for the output display

--- exer.js
+++ exer.js
@@ -1,11 +1,36 @@
 function run(){
 
-  document.getElementById('output').innerHTML =  
+  const template = document.getElementById('output').innerHTML =  
   truncate('We are doing JS string exercises.') + " <br> " + 
   truncate('We are doing JS string exercises.',19) +"<br> "+ 
-  truncate('We are doing JS string exercises.',15,'!!');
+  truncate('We are doing JS string exercises.',15,'!!') +"<br> "+ 
+  truncate('We are doing JS string exercises.',31);
 
 }
+
+// Use the Tooltip but it showed in the displayed output
+//not the specific "..." words 
+tippy('#output', {
+  content: 'We are doing JS string exercises.',
+
+});
+
+
+//Shorten the code and put an additional condition
+function truncate(str, limit, ending) {
+  if (limit && ending == null) {
+      ending = '...';
+     
+  }
+  if (str.length > limit) str = str.substring
+     (0, limit - ending.length) + ending;
+      return str;
+
+};
+
+
+
+
 
 
 // First solution but the ... is not count as an array
@@ -13,19 +38,7 @@ function run(){
 //   if(str.length >= limit){
 //     return str.substring(0, limit) + "..."
 //   }else{
-//     return str;
+//     return str; 
 //   }
 // }
-
-
-//Shorten the code and put an additional condition
-
-function truncate(str, limit, ending) {
-  if (limit && ending == null) {
-    ending = '...';
-  }
-  if (str.length > limit) str = str.substring(0, limit - ending.length) + ending;
-  return str;
-};
-
 
